2376322b3210dc5e3a6d765899b43e6d9fa478c9
BrahianCano <brahianestiven08@gmail.com>
2020-07-07 22:47:38 -0500

    Cambio de rutas 4

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -10,7 +10,7 @@ const NewClient = () => {
                <div className="jumbotron">
                <h1 className="display-4">Agrega un nuevo cliente</h1>
                <p className="lead">Este cliente sera almacenado de manera local, es decir que solo se mostrara en este computador</p>
-               <Link className="btn btn-secondary" to="/dashboard" role="button">Gestionar mis clientes</Link>
+               <Link className="btn btn-secondary" to="/coexitoapp/dashboard" role="button">Gestionar mis clientes</Link>
           </div>
           <h4 className="text-center m-5">COMPLETA TODOS LOS DATOS DEL FORMULARIO</h4>
                <FormClientComponent />
