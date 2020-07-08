e3bab8548d0c4cde6ee1f5fec1769c0b582236f0
BrahianCano <brahianestiven08@gmail.com>
2020-07-07 22:10:44 -0500

    Updates

---
+++ precache-manifest.640e75e08fd5dccf485b85c58c655e75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9349675e9584937bd25c6cb823cbb23",
+    "url": "/coexitoapp/index.html"
+  },
+  {
+    "revision": "880e5248bdeea19fd5ae",
+    "url": "/coexitoapp/static/js/2.517f5f9c.chunk.js"
+  },
+  {
+    "revision": "a41c879f8bdb54796b2468bd148c705b",
+    "url": "/coexitoapp/static/js/2.517f5f9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1ad6c463b41fee1b98",
+    "url": "/coexitoapp/static/js/main.be6d1dae.chunk.js"
+  },
+  {
+    "revision": "bbb578aa58044fb1f276",
+    "url": "/coexitoapp/static/js/runtime-main.9991c5b1.js"
+  }
+]);
